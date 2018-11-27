127a88652dc6f430c37f00103d2480c983ec5bf7
Matthew G. Saroff <msaroff2007@gmail.com>
2018-11-27 23:43:48 +0000

    Primarily Updates to the Settings Pages

--- settings/custom_tags.js
+++ settings/custom_tags.js
@@ -1,15 +1,29 @@
 customMenus = JSON.parse(localStorage.getItem('customMenus'));
 
+// id="saveTagOrder"  Save tag order button
+document.getElementById('saveTagOrder').addEventListener("click",saveTagOrd);
 
-//function myFunction() {
+function saveTagOrd (){
+menuOrdTest = "";
+for (i = 0; i < customMenus.length; i++) {
+menuOrdTest += '\n'+customMenus[i].menuId+"\t"
+menuOrdTest += customMenus[i].menuTitle+"\t"
+menuOrdTest += customMenus[i].parentId+"\t"
+menuOrdTest += customMenus[i].menuArg;
+}
+//console.log("Menu Order\nMenu ID Menu \tTitle \tParent ID \tMenu Argument"+menuOrdTest);
+alert("Menu Order \nMenu ID \t\t\t\Menu Title\t\t\tParent ID\t\tMenu Argument"+menuOrdTest);
+}
+
 var tableRef = document.getElementById("customTagTable").id;
+var ulSort = document.getElementById("listOrder");
+console.log(ulSort);
     for (i = 0; i < Object.keys(customMenus).length; i++) {
     let row = customTagList.insertRow();
     let rowId = customMenus[i].menuId;
     row.setAttribute('id', rowId);
-//    row.setAttribute('data-href', 'https://duckduckgo.com');
-//    row.setAttribute('class', 'clickable');
-//    row.setAttribute('onclick','getClicked(rowID)');
+    row.setAttribute('draggable', true);
+
 //console.log("xyzzy ",row);
 var addCell = row.insertCell(0);
   var newText  = document.createTextNode(customMenus[i].menuArg)
@@ -23,6 +37,44 @@ var addCell = row.insertCell(0);
 var addCell = row.insertCell(0);
   var newText  = document.createTextNode(customMenus[i].menuId)
   addCell.appendChild(newText);
+
+const containerDiv = document.createElement('div');
+      containerDiv.classList.add("grid-container");
+const menuIdDiv = document.createElement('div');
+      menuIdDiv.classList.add("grid-item-menuId");
+const menuTitleDiv = document.createElement('div');
+      menuTitleDiv.classList.add("grid-item-menuTitle");
+const parentIdDiv = document.createElement('div');
+      parentIdDiv.classList.add("grid-item-parentId");
+const menuArgDiv = document.createElement('div');
+      menuArgDiv.classList.add("grid-item-menuArg");
+    var listId = customMenus[i].menuId + "-lst";
+//console.log(menuIdDiv);
+    var createLi = document.createElement("li"); 
+    createLi.setAttribute('id', listId);
+    createLi.setAttribute('draggable', true);
+/*
+grid-item-menuTitle
+grid-item-parentId
+grid-item-menuArg
+grid-container
+*/
+    
+    menuIdDiv.appendChild(document.createTextNode(customMenus[i].menuId));
+    menuTitleDiv.appendChild(document.createTextNode(customMenus[i].menuTitle));
+    parentIdDiv.appendChild(document.createTextNode(customMenus[i].parentId));
+    menuArgDiv.appendChild(document.createTextNode(customMenus[i].menuArg));
+console.log(menuIdDiv,menuTitleDiv);
+
+    containerDiv.appendChild(menuIdDiv);
+    containerDiv.appendChild(menuTitleDiv);
+    containerDiv.appendChild(parentIdDiv);
+    containerDiv.appendChild(menuArgDiv);
+    createLi.appendChild(containerDiv);
+//console.log(createLi);
+ 
+
+  ulSort.appendChild(createLi);
 }
 
 
