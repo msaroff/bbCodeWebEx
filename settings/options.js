80540464ce5b54b287c19666f8044dfad92f3a91
Matthew G. Saroff <msaroff2007@gmail.com>
2018-12-14 18:11:12 -0500

    Daily Backup

--- settings/options.js
+++ settings/options.js
@@ -75,3 +75,12 @@ enableXHTML
 enableMarkDown
 enableCustom
 */
+
+// ----------------- Internationalization ------------------
+// From https://github.com/erosman/HTML-Internationalization
+for (let node of document.querySelectorAll('[data-i18n]')) {
+  let [text, attr] = node.dataset.i18n.split('|');
+  text = chrome.i18n.getMessage(text);
+  attr ? node[attr] = text : node.appendChild(document.createTextNode(text));
+}
+// ----------------- /Internationalization -----------------
