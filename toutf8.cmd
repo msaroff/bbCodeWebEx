ba2bb028e8199a0dd2e04979af7b28f4630b82f3
msaroff <msaroff2007@gmail.com>
2018-12-29 14:27:39 -0500

    Update toutf8.cmd

--- toutf8.cmd
+++ toutf8.cmd
@@ -1,2 +1,7 @@
 rem excel does not export as UTF 8.  This run in powershell does
-get-content .\locale_bbcode.csv | out-file -encoding utf8 locale.csv
+rem this takes a file and outputs it as utf-8 in the utf8 subdirectory
+powershell "get-content C:\Moo\bbCodeWebex_Temp\de.csv | out-file -encoding utf8 C:\Moo\bbCodeWebex_Temp\utf8\de.csv"
+pause
+dir /p
+pause
+
