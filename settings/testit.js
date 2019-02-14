73aefb2ad07a37d00d65eac3311f4c327dc152b0
Matthew G. Saroff <msaroff2007@gmail.com>
2019-02-14 12:49:44 -0500

    working on bbcodextra importy

--- settings/testit.js
+++ settings/testit.js
@@ -2,7 +2,9 @@
 //colorArg = "{{zzGetColor,\"[color=\",\"]\",\"[/color]\"}}texttoformat{{zzColorEnd}}";
 //colorCall = "nocolor";
 const importButton = document.getElementById("bbcodeimport");
-importButton.addEventListener("change", JSONtoVar);
+const callButton = document.getElementById("bbcodeimportclick");
+importButton.addEventListener("change", loadBBCodeXtra);
+callButton.addEventListener("click", trigImport);
 
 //var custProm = "";
 
@@ -10,7 +12,7 @@ importButton.addEventListener("change", JSONtoVar);
 
 
 
-async function JSONtoVar (event) {
+function loadBBCodeXtra (event) {
 //	let customMenu = await custProm;
   var getfile = event.target.files;
   
@@ -21,11 +23,34 @@ async function JSONtoVar (event) {
     // The file's text will be printed here
     let readValue = event.target.result;
     document.getElementById("loaded").innerHTML = "<xmp>"+readValue+"</xmp>";
-	document.getElementById("processed").innerHTML = "processedxyzzy";
+	let forImport = procCustBBCXtags(readValue);
+	document.getElementById("processed").innerHTML = "processedxyzzy<br>"+forImport;
 //browser.storage.local.set({ defCust: readValue }); //store order of custom tags locally
 //location.reload(); // reload page, which reloads custom tags from storage
   };
   //console.log(event.target.result);
   reader.readAsText(file);
  
+}
+
+function procCustBBCXtags (oldCodes){
+	let oldTags = JSON.parse(oldCodes);
+	console.log(JSON.stringify(oldTags,null,2).substring(0,200));
+	let sortedTags = oldTags.sort(function(a, b){
+		return a.name > b.name;
+	});
+	let toImport = {}; //initialize variable to be imported.
+	console.log(JSON.stringify(sortedTags,null,2).substring(0,100));
+	for (i = 0; i < Object.keys(sortedTags).length; i++){ // go through the entire array
+		if (sortedTags[i].name.includes("custom") && sortedTags[i].name.includes("action")){
+			console.log(sortedTags[i].name);
+		}
+	}
+		
+	
+	return ("procCustBBCXtags");
+}
+
+async function trigImport () {
+	importButton.click();
 }
