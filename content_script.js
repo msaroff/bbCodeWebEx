0d1af8d748c69ccb454c57a93db490760fa15b48
Matthew G. Saroff <msaroff2007@gmail.com>
2019-03-21 19:23:35 -0400

    Update content_script.js

--- content_script.js
+++ content_script.js
@@ -151,6 +151,7 @@ async function colorPick (colorArg){ //read the color from the popup
         // some text boxes do not have an id assigned, but they do have a name assigned, if so, use the name
 //        let FocusInfo = document.getElementById(document.activeElement.id).contentWindow.document.body.innerHTML;
         let txtcont = document.activeElement.value; //contents of edit box
+		console.log(txtcont);
 		if (txtcont === undefined) { // occurs when using the context menu on a rich text edit box
 			txtcont = document.activeElement.contentWindow.document.body.innerHTML;
 			console.log(txtcont);
