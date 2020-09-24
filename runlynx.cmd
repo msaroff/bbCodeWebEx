af570f7c22c285d44409ecc23a3c6195b6eb7ee2
Matthew G. Saroff <msaroff2007@gmail.com>
2020-09-24 16:12:42 -0400

    aa

--- runlynx.cmd
+++ runlynx.cmd
@@ -1,3 +1,2 @@
 rem this is simply a way to call lynx in a simplified manner
 %userprofile%\Documents\github\bbcodewebex\Lynx\lynx.exe -dump -width 300 -cfg=%userprofile%\Documents\github\bbcodewebex\lynx\lynx.cfg %1 
-pause
