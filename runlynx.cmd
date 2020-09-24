a023bed14496a447bb6d249736b295debceb26f8
Matthew G. Saroff <msaroff2007@gmail.com>
2020-09-24 16:30:00 -0400

    aa

--- runlynx.cmd
+++ runlynx.cmd
@@ -1,3 +1,4 @@
+@echo off
 @rem this is simply a way to call lynx in a simplified manner
 @rem set to dump output to file in UTF-8 and width prevents clipping
 %userprofile%\Documents\github\bbcodewebex\Lynx\lynx.exe -dump -width 300 -cfg=%userprofile%\Documents\github\bbcodewebex\lynx\lynx.cfg %1 
