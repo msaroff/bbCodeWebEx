914cad66ec8270d687f35138e3ca819366d0c8d4
msaroff <msaroff2007@gmail.com>
2020-09-24 21:37:59 -0400

    a

--- update_jsons.cmd
+++ update_jsons.cmd
@@ -4,23 +4,23 @@
 @rem it then takes the files and dumps them to the appropriate add-on
 @rem directory.
 @ rem %userprofile%\Documents\github\bbcodewebex\Lynx\lynx.exe -cfg=%userprofile%\Documents\github\bbcodewebex\lynx\lynx.cfg -dump -width 300 .\DefMenu.htm > DefMenu.json
-call runlynx C:\Moo\bbCodeWebex_Temp\en-US.htm > "%userprofile%\Documents\GitHub\bbCodeWebEx\_locales\en-US\messages.json"
-call runlynx C:\Moo\bbCodeWebex_Temp\de.htm > "%userprofile%\Documents\GitHub\bbCodeWebEx\_locales\de\messages.json"
-call runlynx C:\Moo\bbCodeWebex_Temp\es-ES.htm > "%userprofile%\Documents\GitHub\bbCodeWebEx\_locales\es-ES\messages.json"
-call runlynx C:\Moo\bbCodeWebex_Temp\fi.htm > "%userprofile%\Documents\GitHub\bbCodeWebEx\_locales\fi\messages.json"
-call runlynx C:\Moo\bbCodeWebex_Temp\fr.htm > "%userprofile%\Documents\GitHub\bbCodeWebEx\_locales\fr\messages.json"
-call runlynx C:\Moo\bbCodeWebex_Temp\he.htm > "%userprofile%\Documents\GitHub\bbCodeWebEx\_locales\he\messages.json"
-call runlynx C:\Moo\bbCodeWebex_Temp\it.htm > "%userprofile%\Documents\GitHub\bbCodeWebEx\_locales\it\messages.json"
-call runlynx C:\Moo\bbCodeWebex_Temp\ja.htm > "%userprofile%\Documents\GitHub\bbCodeWebEx\_locales\ja\messages.json"
-call runlynx C:\Moo\bbCodeWebex_Temp\ko.htm > "%userprofile%\Documents\GitHub\bbCodeWebEx\_locales\ko\messages.json"
-call runlynx C:\Moo\bbCodeWebex_Temp\pl.htm > "%userprofile%\Documents\GitHub\bbCodeWebEx\_locales\pl\messages.json"
-call runlynx C:\Moo\bbCodeWebex_Temp\pt-BR.htm > "%userprofile%\Documents\GitHub\bbCodeWebEx\_locales\pt-BR\messages.json"
-call runlynx C:\Moo\bbCodeWebex_Temp\pt-PT.htm > "%userprofile%\Documents\GitHub\bbCodeWebEx\_locales\pt-PT\messages.json"
-call runlynx C:\Moo\bbCodeWebex_Temp\ru.htm > "%userprofile%\Documents\GitHub\bbCodeWebEx\_locales\ru\messages.json"
-call runlynx C:\Moo\bbCodeWebex_Temp\sk.htm > "%userprofile%\Documents\GitHub\bbCodeWebEx\_locales\sk\messages.json"
-call runlynx C:\Moo\bbCodeWebex_Temp\sr.htm > "%userprofile%\Documents\GitHub\bbCodeWebEx\_locales\sr\messages.json"
-call runlynx C:\Moo\bbCodeWebex_Temp\th.htm > "%userprofile%\Documents\GitHub\bbCodeWebEx\_locales\th\messages.json"
-call runlynx C:\Moo\bbCodeWebex_Temp\zh-TW.htm > "%userprofile%\Documents\GitHub\bbCodeWebEx\_locales\zh-TW\messages.json"
-call runlynx C:\Moo\bbCodeWebex_Temp\DefMenu.htm > "%userprofile%\Documents\GitHub\bbCodeWebEx\data\DefMenu.json"
+call runlynx C:\Moo\bbCodeWebex_Temp\en-US.htm > C:\Moo\bbCodeWebex_Temp\utf8\en-US.json
+call runlynx C:\Moo\bbCodeWebex_Temp\de.htm > C:\Moo\bbCodeWebex_Temp\utf8\de.json
+call runlynx C:\Moo\bbCodeWebex_Temp\es-ES.htm > C:\Moo\bbCodeWebex_Temp\utf8\es-ES.json
+call runlynx C:\Moo\bbCodeWebex_Temp\fi.htm > C:\Moo\bbCodeWebex_Temp\utf8\fi.json
+call runlynx C:\Moo\bbCodeWebex_Temp\fr.htm > C:\Moo\bbCodeWebex_Temp\utf8\fr.json
+call runlynx C:\Moo\bbCodeWebex_Temp\he.htm > C:\Moo\bbCodeWebex_Temp\utf8\he.json
+call runlynx C:\Moo\bbCodeWebex_Temp\it.htm > C:\Moo\bbCodeWebex_Temp\utf8\it.json
+call runlynx C:\Moo\bbCodeWebex_Temp\ja.htm > C:\Moo\bbCodeWebex_Temp\utf8\ja.json
+call runlynx C:\Moo\bbCodeWebex_Temp\ko.htm > C:\Moo\bbCodeWebex_Temp\utf8\ko.json
+call runlynx C:\Moo\bbCodeWebex_Temp\pl.htm > C:\Moo\bbCodeWebex_Temp\utf8\pl.json
+call runlynx C:\Moo\bbCodeWebex_Temp\pt-BR.htm > C:\Moo\bbCodeWebex_Temp\utf8\pt-BR.json
+call runlynx C:\Moo\bbCodeWebex_Temp\pt-PT.htm > C:\Moo\bbCodeWebex_Temp\utf8\pt-PT.json
+call runlynx C:\Moo\bbCodeWebex_Temp\ru.htm > C:\Moo\bbCodeWebex_Temp\utf8\ru.json
+call runlynx C:\Moo\bbCodeWebex_Temp\sk.htm > C:\Moo\bbCodeWebex_Temp\utf8\sk.json
+call runlynx C:\Moo\bbCodeWebex_Temp\sr.htm > C:\Moo\bbCodeWebex_Temp\utf8\sr.json
+call runlynx C:\Moo\bbCodeWebex_Temp\th.htm > C:\Moo\bbCodeWebex_Temp\utf8\th.json
+call runlynx C:\Moo\bbCodeWebex_Temp\zh-TW.htm > C:\Moo\bbCodeWebex_Temp\utf8\zh-TW.json
+call runlynx C:\Moo\bbCodeWebex_Temp\DefMenu.htm > C:\Moo\bbCodeWebex_Temp\utf8\DefMenu.json
 pause
 
