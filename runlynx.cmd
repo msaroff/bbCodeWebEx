6c46f4e7a1a7112f21f207646e27bdb423e2fefb
msaroff <msaroff2007@gmail.com>
2020-09-24 23:12:51 -0400

    lfksfd;

--- runlynx.cmd
+++ runlynx.cmd
@@ -1,4 +1,3 @@
-@echo off
 @rem this is simply a way to call lynx in a simplified manner
 @rem set to dump output to file in UTF-8 and width prevents clipping
-"%userprofile%\Documents\github\bbcodewebex\Lynx\lynx.exe" -dump -width 300 -cfg="%userprofile%\Documents\github\bbcodewebex\lynx\lynx.cfg" %1 
+"%userprofile%\Documents\github\bbcodewebex\Lynx\lynx.exe" -dump -width 300 -display_charset UTF-8 -cfg="%userprofile%\Documents\github\bbcodewebex\lynx\lynx.cfg" %1 > %2
--- update_jsons.cmd
+++ update_jsons.cmd
@@ -3,24 +3,25 @@
 @rem Runlynx to deal with long directory names to call Browser
 @rem it then takes the files and dumps them to the appropriate add-on
 @rem directory.
-@ rem %userprofile%\Documents\github\bbcodewebex\Lynx\lynx.exe -cfg=%userprofile%\Documents\github\bbcodewebex\lynx\lynx.cfg -dump -width 300 .\DefMenu.htm > DefMenu.json
-call runlynx C:\Moo\bbCodeWebex_Temp\en-US.htm > C:\Moo\bbCodeWebex_Temp\utf8\en-US.json
-call runlynx C:\Moo\bbCodeWebex_Temp\de.htm > C:\Moo\bbCodeWebex_Temp\utf8\de.json
-call runlynx C:\Moo\bbCodeWebex_Temp\es-ES.htm > C:\Moo\bbCodeWebex_Temp\utf8\es-ES.json
-call runlynx C:\Moo\bbCodeWebex_Temp\fi.htm > C:\Moo\bbCodeWebex_Temp\utf8\fi.json
-call runlynx C:\Moo\bbCodeWebex_Temp\fr.htm > C:\Moo\bbCodeWebex_Temp\utf8\fr.json
-call runlynx C:\Moo\bbCodeWebex_Temp\he.htm > C:\Moo\bbCodeWebex_Temp\utf8\he.json
-call runlynx C:\Moo\bbCodeWebex_Temp\it.htm > C:\Moo\bbCodeWebex_Temp\utf8\it.json
-call runlynx C:\Moo\bbCodeWebex_Temp\ja.htm > C:\Moo\bbCodeWebex_Temp\utf8\ja.json
-call runlynx C:\Moo\bbCodeWebex_Temp\ko.htm > C:\Moo\bbCodeWebex_Temp\utf8\ko.json
-call runlynx C:\Moo\bbCodeWebex_Temp\pl.htm > C:\Moo\bbCodeWebex_Temp\utf8\pl.json
-call runlynx C:\Moo\bbCodeWebex_Temp\pt-BR.htm > C:\Moo\bbCodeWebex_Temp\utf8\pt-BR.json
-call runlynx C:\Moo\bbCodeWebex_Temp\pt-PT.htm > C:\Moo\bbCodeWebex_Temp\utf8\pt-PT.json
-call runlynx C:\Moo\bbCodeWebex_Temp\ru.htm > C:\Moo\bbCodeWebex_Temp\utf8\ru.json
-call runlynx C:\Moo\bbCodeWebex_Temp\sk.htm > C:\Moo\bbCodeWebex_Temp\utf8\sk.json
-call runlynx C:\Moo\bbCodeWebex_Temp\sr.htm > C:\Moo\bbCodeWebex_Temp\utf8\sr.json
-call runlynx C:\Moo\bbCodeWebex_Temp\th.htm > C:\Moo\bbCodeWebex_Temp\utf8\th.json
-call runlynx C:\Moo\bbCodeWebex_Temp\zh-TW.htm > C:\Moo\bbCodeWebex_Temp\utf8\zh-TW.json
-call runlynx C:\Moo\bbCodeWebex_Temp\DefMenu.htm > C:\Moo\bbCodeWebex_Temp\utf8\DefMenu.json
+@rem %userprofile%\Documents\github\bbcodewebex\Lynx\lynx.exe 
+@rem -cfg=%userprofile%\Documents\github\bbcodewebex\lynx\lynx.cfg -dump -width 300 .\DefMenu.htm > DefMenu.json
+call runlynx "C:\Moo\bbCodeWebex_Temp\en-US.htm" C:\Moo\bbCodeWebex_Temp\utf8\en-US.json
+call runlynx "C:\Moo\bbCodeWebex_Temp\de.htm" C:\Moo\bbCodeWebex_Temp\utf8\de.json
+call runlynx "C:\Moo\bbCodeWebex_Temp\es-ES.htm" C:\Moo\bbCodeWebex_Temp\utf8\es-ES.json
+call runlynx "C:\Moo\bbCodeWebex_Temp\fi.htm" C:\Moo\bbCodeWebex_Temp\utf8\fi.json
+call runlynx "C:\Moo\bbCodeWebex_Temp\fr.htm" C:\Moo\bbCodeWebex_Temp\utf8\fr.json
+call runlynx "C:\Moo\bbCodeWebex_Temp\he.htm" C:\Moo\bbCodeWebex_Temp\utf8\he.json
+call runlynx "C:\Moo\bbCodeWebex_Temp\it.htm" C:\Moo\bbCodeWebex_Temp\utf8\it.json
+call runlynx "C:\Moo\bbCodeWebex_Temp\ja.htm" C:\Moo\bbCodeWebex_Temp\utf8\ja.json
+call runlynx "C:\Moo\bbCodeWebex_Temp\ko.htm" C:\Moo\bbCodeWebex_Temp\utf8\ko.json
+call runlynx "C:\Moo\bbCodeWebex_Temp\pl.htm" C:\Moo\bbCodeWebex_Temp\utf8\pl.json
+call runlynx "C:\Moo\bbCodeWebex_Temp\pt-BR.htm" C:\Moo\bbCodeWebex_Temp\utf8\pt-BR.json
+call runlynx "C:\Moo\bbCodeWebex_Temp\pt-PT.htm" C:\Moo\bbCodeWebex_Temp\utf8\pt-PT.json
+call runlynx "C:\Moo\bbCodeWebex_Temp\ru.htm" C:\Moo\bbCodeWebex_Temp\utf8\ru.json
+call runlynx "C:\Moo\bbCodeWebex_Temp\sk.htm" C:\Moo\bbCodeWebex_Temp\utf8\sk.json
+call runlynx "C:\Moo\bbCodeWebex_Temp\sr.htm" C:\Moo\bbCodeWebex_Temp\utf8\sr.json
+call runlynx "C:\Moo\bbCodeWebex_Temp\th.htm" C:\Moo\bbCodeWebex_Temp\utf8\th.json
+call runlynx "C:\Moo\bbCodeWebex_Temp\zh-TW.htm" C:\Moo\bbCodeWebex_Temp\utf8\zh-TW.json
+call runlynx "C:\Moo\bbCodeWebex_Temp\DefMenu.htm" C:\Moo\bbCodeWebex_Temp\utf8\DefMenu.json
 pause
 
